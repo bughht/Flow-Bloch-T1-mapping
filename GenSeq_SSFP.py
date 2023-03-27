Fixed several bugs: sign altered phase and gradient

GenSeq_SSFP.py
@@ -13,12 +13,13 @@ class bSSFP:
         FOV: list,  # mm
         k_shape: list,
         slice_thickness: float,  # TODO: change it into bandwidth
+        k_center_first: bool,
         sign_alter: bool,
         preparation: bool
     ):
         self.seq = []
-        self.TR, self.FA, self.FOV, self.k_shape, self.slice_thickness, self.sign_alter, self.preparation = TR, FA, np.array(
-            FOV), np.array(k_shape), slice_thickness, sign_alter, preparation
+        self.TR, self.FA, self.FOV, self.k_shape, self.slice_thickness, self.k_center_first, self.sign_alter, self.preparation = TR, FA, np.array(
+            FOV), np.array(k_shape), slice_thickness, k_center_first, sign_alter, preparation
         self.data_process()
 
     def data_process(self):
@@ -30,8 +31,10 @@ class bSSFP:
         # [self.k_shape[0]/self.FOV[0], self.k_shape[1]/self.FOV[1]])
 
         # T/mm
-        self.GX = self.k_shape[0]/(2*np.pi*gamma*self.TR/4)*1e9
-        self.GY_max = self.k_shape[1]/(2*np.pi*gamma*self.TR/4)*1e9
+        # self.GX = self.k_shape[0]/(2*np.pi*gamma*self.TR/4)*1e9
+        # self.GY_max = self.k_shape[1]/(2*np.pi*gamma*self.TR/4)*1e9
+        self.GX = self.k_shape[0]/(2*gamma*self.FOV[0]*self.TR/4)*1e3
+        self.GY_max = self.k_shape[1]/(2*gamma*self.FOV[1]*self.TR/4)*1e3
 
     def add_readout(self, t_start, FA, TR, GX, GY, kY_idx):
         FA,  GX, GY, kY_idx = float(FA),  float(GX), float(GY), int(kY_idx)
@@ -88,6 +91,25 @@ class bSSFP:
         self.seq.append(GY_tx_2)
         self.seq.append(GX_tx_2)
 
+    def get_twisted_index(self, N):
+        center_idx = N//2
+        idx_list = [center_idx]
+
+        twist_direction = -1
+        twist_cnt = False
+        twist_step = 1
+
+        for i in range(1, N):
+            next_idx = center_idx+twist_direction*twist_step
+            idx_list.append(next_idx)
+
+            twist_direction *= -1
+            if twist_cnt:
+                twist_step += 1
+            twist_cnt = not twist_cnt
+
+        return idx_list
+
     def generate(self, name):
         t_prep = 0
         if self.preparation:
@@ -97,20 +119,28 @@ class bSSFP:
                 "type": "PULSE",
                 "FA": self.FA/2,
             })
-        for readout_idx in range(self.NFlip):
-            t_start = t_prep+self.TR*readout_idx
+        if not self.k_center_first:
+            self.readout_kY_idx = range(self.NFlip)
+        else:
+            self.readout_kY_idx = self.get_twisted_index(self.NFlip)
+        for readout_idx, readout_kY in enumerate(self.readout_kY_idx):
+            t_start = float(t_prep+self.TR*readout_idx)
             self.add_readout(
                 t_start,
                 self.FA*(((readout_idx % 2)*2-1) if self.sign_alter else 1),
                 self.TR,
                 self.GX,
-                -self.GY_max+readout_idx*2*self.GY_max/(self.NFlip-1),
-                readout_idx
+                -self.GY_max+readout_kY*2*self.GY_max/(self.NFlip-1),
+                readout_kY
             )
         with open(os.path.join("sequences_ssfp", name+".yaml"), "w") as f:
             yaml.dump(self.seq, f)
 
 
 if __name__ == "__main__":
-    ssfp = bSSFP(2.8, 5, [314, 314], [128, 128], 5, True, True)
-    ssfp.generate("TR2.8_FA5")
+    # ssfp = bSSFP(2.8, 50, [330, 330], [128, 128], 10, True, True, True)
+    # ssfp.generate("TR2.8_FA50_FOV330_K128_center_first")
+    ssfp = bSSFP(2.8, 90, [500, 500], [64, 64], 10, True, True, True)
+    ssfp.generate("TR2.8_FA90_FOV500_K64_center_first")
+    ssfp = bSSFP(2.8, 90, [500, 500], [64, 64], 10, False, True, True)
+    ssfp.generate("TR2.8_FA90_FOV500_K64")

SeqSimulator.py
@@ -5,6 +5,7 @@ import BlochSim as bs
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+gamma = 42.577478518e6  # Hz/T
 
 
 class M:
@@ -19,7 +20,7 @@ class M:
         self.M = Rflip@self.M
 
     def freeprecess(self, t, Gx, Gy):
-        df = Gx*self.pos[0]+Gy*self.pos[1]
+        df = gamma*(Gx*self.pos[0]+Gy*self.pos[1])
         Afp, Bfp = bs.freeprecess(t, self.T1, self.T2, df)
         self.M = Afp @ self.M+Bfp
 
@@ -31,18 +32,27 @@ class M:
 
 
 if __name__ == "__main__":
-    seq = MRISequence(os.path.join("sequences_ssfp", "TR2.8_FA5.yaml")).data
-    m_test = M(1001, 20, np.array([32, -3]))
+    seq = MRISequence(os.path.join("sequences_ssfp",
+                                   "TR2.8_FA20_FOV500_K64_center_first.yaml")).data
+    #    "TR2.8_FA20_FOV500_K64.yaml")).data
+    ratio = 500/64
+    x_rate = 1
+    y_rate = 1
+    m_test = M(1000, 30, np.array([20*ratio*x_rate, -0*ratio*y_rate]))
 
     t = []
     M = []
     t_adc = []
     M_adc = []
-    k = np.zeros((128, 128), dtype=complex)
+    Gx_h = []
+    Gy_h = []
+    k = np.zeros((64, 64), dtype=complex)
 
     t_local = 0
     Gx = 0
     Gy = 0
+    cnt_adc = 0
+    now_adc = False
     for ts in seq:
         # print(ts)
         m_test.freeprecess(ts['t']-t_local, Gx, Gy)
@@ -54,24 +64,43 @@ if __name__ == "__main__":
             Gy = ts['G']
         t_local = ts['t']
         if ts['type'] == "ADC":
+            if not now_adc:
+                now_adc = True
+                cnt_adc += 1
             k[ts['kx'], ts['ky']] = m_test.M[0]+1j*m_test.M[1]
+            if (cnt_adc % 2 == 0):
+                k[ts['kx'], ts['ky']] *= np.exp(1j*np.pi)
             t_adc.append(t_local)
             M_adc.append(m_test.M)
+        else:
+            if now_adc:
+                now_adc = False
         t.append(t_local)
+        Gx_h.append(Gx)
+        Gy_h.append(Gy)
         M.append(m_test.M)
+
+    plt.figure()
+    k_meshgrid_x, k_meshgrid_y = np.meshgrid(range(64), range(64))
+    Ex = k[k_meshgrid_x, k_meshgrid_y].real
+    Ey = k[k_meshgrid_x, k_meshgrid_y].imag
+    plt.quiver(k_meshgrid_x, k_meshgrid_y, Ex, Ey)
 
     plt.figure()
     M = np.array(M)
     M_adc = np.array(M_adc)
-    plt.subplot(3, 1, 1)
+    plt.subplot(4, 1, 1)
     plt.plot(t, M[:, 2])
     plt.plot(t_adc, M_adc[:, 2])
-    plt.subplot(3, 1, 2)
+    plt.subplot(4, 1, 2)
     plt.plot(t, np.abs(M[:, 0]+1j*M[:, 1]))
     plt.plot(t_adc, np.abs(M_adc[:, 0]+1j*M_adc[:, 1]))
-    plt.subplot(3, 1, 3)
+    plt.subplot(4, 1, 3)
     plt.plot(t, np.angle(M[:, 0]+1j*M[:, 1]))
     plt.plot(t_adc, np.angle(M_adc[:, 0]+1j*M_adc[:, 1]))
+    plt.subplot(4, 1, 4)
+    plt.plot(t, Gx_h)
+    plt.plot(t, Gy_h)
 
     plt.figure()
     plt.subplot(1, 2, 1)
@@ -80,5 +109,5 @@ if __name__ == "__main__":
     # plt.imshow(np.angle(k))
     plt.subplot(1, 2, 2)
     # plt.imshow(np.log(np.abs(sfft.ifft2(k))))
-    plt.imshow(np.abs(sfft.ifft2(k)))
+    plt.imshow(np.abs(sfft.fftshift(sfft.ifft2(k))))
     plt.show()